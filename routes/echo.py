7d02b3c989e414b939d1e62ce374955718abb46d
Nasri Adzlani <nasriadzlani@live.com>
2025-01-14 23:07:44 +0700

    prompts

--- routes/echo.py
+++ routes/echo.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 from model.echo_model import PromptModel
 from services.echo import prompt_generator
+from services.user import check_user
 
 router = APIRouter(
     prefix="/echo",
@@ -16,6 +17,10 @@ async def init_world():
 
 @router.post("/prompt")
 async def prompt(prompt_model : PromptModel):
-    return prompt_generator(prompt_model.message, "user", prompt_model.username)
-    
+    # check if user exists
+    try:
+        check_user(prompt_model.username)
+        return prompt_generator(prompt_model.message, "user", prompt_model.username)
+    except Exception as e:
+        return {"error": str(e)}
     
